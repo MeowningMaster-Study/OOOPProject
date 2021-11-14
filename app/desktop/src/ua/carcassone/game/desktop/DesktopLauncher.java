be56652985005aa976bf3f907a1c71d0c6592f7b
eVQruJNq <leirimnad@gmail.com>
2021-11-14 14:42:18 +0200

    setup libgdx project

---
+++ app/desktop/src/ua/carcassone/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ua.carcassone.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ua.carcassone.game.CarcassoneGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CarcassoneGame(), config);
+	}
+}
